6b831c568e957bc5bfa1fb4658fd890fd34ed5c4
Alex Yang <koopakondra@gmail.com>
2026-03-23 21:41:34 -0700

    Adjust cache intervals and fix cache deletion logic

--- app/v0/[[...path]]/route.ts
+++ app/v0/[[...path]]/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest } from "next/server";
 
 const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-const REFRESH_INTERVAL = 15 * 60 * 1000; // 15 minutes
-const FORGET_INTERVAL = 2.9 * 24 * 60 * 60 * 1000; // 3 days
+const REFRESH_INTERVAL = 15 * 60 * 1000; // 5 minutes
+const FORGET_INTERVAL = 6.9 * 24 * 60 * 60 * 1000; // 7 days
 
 let cache: { [referrer: string]: { [url: string]: any } } = {};
 let timestamps: { [referrer: string]: { [url: string]: number } } = {};
@@ -136,10 +136,10 @@ async function mergePaginatedData(referrerHostname: string) {
 }
 
 const PREFIX = 'export const cache = ';
-const SUFFIX = ';\nwindow.airtableCache = cache; /*';
+const SUFFIX = ';\nwindow.airtableCache = cache;';
 
 async function saveCache(referrerHostname: string) {
-    const cacheString = PREFIX + JSON.stringify(cache[referrerHostname]) + SUFFIX + JSON.stringify(timestamps[referrerHostname]);
+    const cacheString = PREFIX + JSON.stringify(cache[referrerHostname]) + SUFFIX;
 
     const fs = require('fs');
     const path = require('path');
@@ -156,14 +156,15 @@ async function loadCache(referrerHostname: string) {
 
     if (fs.existsSync(cacheFilePath)) {
         const fileContent = fs.readFileSync(cacheFilePath, 'utf8') as string;
-        const jsonString = fileContent.substring(PREFIX.length, fileContent.indexOf(SUFFIX)).trim();
+        const jsonString = fileContent.substring(PREFIX.length, fileContent.lastIndexOf('}') + 1).trim();
         const loadedCache = JSON.parse(jsonString);
 
-        const timestampsString = fileContent.substring(fileContent.indexOf(SUFFIX) + SUFFIX.length).trim();
-        const loadedTimestamps = JSON.parse(timestampsString);
-
         cache[referrerHostname] = loadedCache;
-        timestamps[referrerHostname] = loadedTimestamps;
+        timestamps[referrerHostname] = {};
+
+        for (const url in cache[referrerHostname]) {
+            timestamps[referrerHostname][url] = Date.now();
+        }
 
         console.log('[API] Cache loaded from cache-' + referrerHostname + '.js');
     } else {
@@ -232,8 +233,8 @@ async function refreshCache(url: string, referrerHostname: string) {
     for (const key in cache[referrerHostname]) {
         if (Date.now() - timestamps[referrerHostname][key] > FORGET_INTERVAL) {
             console.log("\n[API] Forgetting cache for unused URL:", decodeURIComponent(key));
-            delete cache[referrerHostname][key];
-            delete timestamps[referrerHostname][key];
+            delete cache[key];
+            delete timestamps[key];
         }
     }
 
